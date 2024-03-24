b090a6603d27a484067cf738ad371d01835b6025
Moustafa <moustafa.metwaly705@outlook.com>
2024-03-25 00:08:48 +0200

    1st task

---
+++ main.js
@@ -0,0 +1,9 @@
+alert("لأيجاد مساحه المستطيل");
+var x =Number(prompt("ادخل طول الضلع"));
+var y =Number(prompt("ادخل عرض الضلع"));
+var area = x * y;
+var primter = 2 * (x + y);
+
+alert("مساحه المستطيل" + area)
+alert("محيط المستطيل" + primter)
+console.log( " مساحه المستطيل " + area +  " محيط المستطيل" + primter);
